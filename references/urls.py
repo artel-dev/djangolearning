31fa146312ec6614825d62c50b5f12d8643c5acf
nezamay <nezamay@mail.ru>
2022-08-02 22:47:10 +0300

    + view, template, url for class Partner

--- references/urls.py
+++ references/urls.py
@@ -11,4 +11,9 @@ urlpatterns = [
     path("user/update/<int:pk>", views.UserUpdateView.as_view(), name="user-update"),
     path("user/delete/<int:pk>", views.UserDeleteView.as_view(), name="user-delete"),
     path("user/<int:pk>", views.UserDetailView.as_view(), name="user-detail"),
+    path("partner", views.PartnerListView.as_view(), name="partner-list"),
+    path("partner/create", views.PartnerCreateView.as_view(), name="partner-create"),
+    path("partner/update/<int:pk>", views.PartnerUpdateView.as_view(), name="partner-update"),
+    path("partner/delete/<int:pk>", views.PartnerDeleteView.as_view(), name="partner-delete"),
+    path("partner/<int:pk>", views.PartnerDetailView.as_view(), name="partner-detail"),
 ]
--- references/views.py
+++ references/views.py
@@ -7,7 +7,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-from .models import User
+from .models import User, Partner
 
 
 # Create your views here.
@@ -120,6 +120,60 @@ class UserDetailView(DetailView):
         actions = [
             {'name': 'user-update', 'title': 'edit'},
             {'name': 'user-delete', 'title': 'delete'},
+        ]
+        context["actions"] = actions
+        return context
+
+
+class PartnerListView(ListView):
+    model = Partner
+    template_name = "references/partner_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        actions = [
+            {'name': 'partner-create', 'title': 'create'},
+        ]
+        context["actions"] = actions
+        return context
+
+
+class PartnerCreateView(CreateView):
+    model = Partner
+    fields = [
+        "name",
+        "description"
+    ]
+    template_name = "references/partner_detail.html"
+    success_url = reverse_lazy('partner-list')
+
+
+class PartnerUpdateView(UpdateView):
+    model = Partner
+    fields = [
+        "name",
+        "description"
+    ]
+    template_name = "references/partner_detail.html"
+    success_url = reverse_lazy('partner-list')
+
+
+class PartnerDeleteView(DeleteView):
+    model = Partner
+    template_name = "references/partner_confirm_delete.html"
+    success_url = reverse_lazy('partner-list')
+
+
+class PartnerDetailView(DetailView):
+    model = Partner
+    template_name = "references/partner_detail.html"
+    success_url = reverse_lazy('partner-list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        actions = [
+            {'name': 'partner-update', 'title': 'edit'},
+            {'name': 'partner-delete', 'title': 'delete'},
         ]
         context["actions"] = actions
         return context
