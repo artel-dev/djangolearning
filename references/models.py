ea0b316811ad786e3a21e23ccc431711a63e6ffa
nezamay <nezamay@mail.ru>
2022-08-02 09:33:51 +0300

    Create class Partner

--- references/models.py
+++ references/models.py
@@ -8,3 +8,8 @@ class User(AbstractUser):
     name = models.CharField(max_length=150, default='')
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+
+
+class Partner(models.Model):
+    name = models.CharField(max_length=150, default='')
+    description = models.CharField(max_length=500)
