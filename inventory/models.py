0a4b3f0956ef5fa701f87fe4c6205d978f04dcae
Vlad-Lavrenko <vlad.lavrenko@gmail.com>
2022-08-14 01:56:16 +0300

    Add Receive

--- inventory/models.py
+++ inventory/models.py
@@ -1,6 +1,6 @@
 
 from django.db import models
-from references.models import User, Product, Company  #Warehouse
+from references.models import User, Product, Company #, Warehouse
 
 
 class PickAndPack(models.Model):
@@ -25,3 +25,23 @@ class PickAndPackLine(models.Model):
     def __str__(self):
         #return f"{self.product}-{self.description} ({self.pick_and_pack})"
         return f"{self.product} ({self.sale_invoice})"
+
+class Receive(models.Model):
+    date = models.DateTimeField()
+    number = models.CharField(max_length=20)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="receive")
+    # warehouse = models.ForeignKey(warehouse,on_delete=models.CASCADE, related_name="receive")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="receive")
+
+    def __str__(self):
+        return f"Receive {self.number} ({self.date})"
+
+class ReceiveLine(models.Model):
+    receive = models.ForeignKey(Receive, on_delete=models.CASCADE, related_name="products")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="receive_lines")
+    qty = models.DecimalField(max_digits=10, decimal_places=3)
+    price = models.DecimalField(max_digits=15, decimal_places=2)
+    amount = models.DecimalField(max_digits=15, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.product}"
